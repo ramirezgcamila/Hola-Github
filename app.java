16df4431e25840c92a28a51b3b2da7894752aec9
Nicolas <117557162+MrZLeviatan@users.noreply.github.com>
2024-03-02 16:25:08 -0500

    Cambios
    Cambios en el app.java

--- app.java
+++ app.java
@@ -2,7 +2,13 @@ public class app {
 
     public static void main(String[] args) {
 
-        System.out.println("hola github");
+        System.out.println("hola github kraken");
+
+        for (int i = 0; i < 10; i++) {
+
+            System.out.println(" Mis huevos" + i);
+
+        }
     }
 
 }
